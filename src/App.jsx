689a387f842ce8520860025b97026e3a2f36cb52
Shantanu Bombatkar <33876955+shaantanu9@users.noreply.github.com>
2022-03-06 15:04:14 +0530

    second commit
    second commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+function Hh(props){
+  var heading = props.heading, list = props.list
+
+  console.log(heading,list)
+
+  return (
+    <div className="ml-3 mt-3">
+    <p className="text-3xl text-blue-800 text-extrabold " >{heading}</p>
+    {list.map((el)=>(<li>{el}</li>))}
+    </div>
+
+  )
+}
+
+
+function App() {
+
+  var list1 = ["android","Blackbeery","iPhone","Windows Phone"];
+  var list2 = ["Samsung","HTC","Micromax","Apple"];
+
+  var heading1 = "Mobile Operating System";
+  var heading2 = "Mobile Manufacturing"; 
+
+  return (
+    <div className='App'>
+    <>
+    <Hh heading={heading1} list={list1} />
+    <Hh heading={heading2} list={list2} />
+    </>
+    </div>
+
+  );
+}
+
+
+export default App;
